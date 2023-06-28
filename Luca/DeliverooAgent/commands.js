7e6db3b1885511c5523ec7879d84289684a5b9c3
LcaZl <luca.zanolo@outlook.it>
2023-06-28 18:48:08 +0200

    Rem

--- Luca/DeliverooAgent/commands.js
+++
@@ -1,9 +0,0 @@
-const commands = {
-    // silence outputs
-    silence_parcels : true,
-    silence_agents : true,
-    silence_player: true,
-    silence_agent_memory : true
-}
-
-export default commands;
