9bfff6fcde832b16926b4f036ea118406b96de32
LcaZl <135067971+LcaZl@users.noreply.github.com>
2023-10-26 15:02:37 +0200

    Delete Project Baseline directory

--- Project Baseline/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-
-    host: "http://localhost:8080", // https://deliveroojs.onrender.com"
-
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4MjQxYTk0NTQyIiwibmFtZSI6Ikx1Y2EiLCJpYXQiOjE2ODc1MjgwMTR9.Jh0wB80hjIiy_EsRTp43duXrd1fmaNNjXyh9ztRF3Aw'
-
-}
-
-export default config;
