5fa683d302894162f5e6e579ad7212ee12a508ce
LcaZl <135067971+LcaZl@users.noreply.github.com>
2023-10-26 15:03:23 +0200

    Delete Luca directory

--- Luca/DeliverooAgent/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-
-    host: "http://localhost:8080", // https://deliveroojs.onrender.com"
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMwYTAyYTBjZDQzIiwibmFtZSI6IlRlc3QxIiwiaWF0IjoxNjg3NjIzNTIwfQ.E_D_zgql6IIaUY-42SvwUtRfVbbJ97-VRXpsVJ8-LAA'
-
-}
-
-export default config;
