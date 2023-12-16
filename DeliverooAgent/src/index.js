45c49a618a4b74619fac46177f7af2d4aa861ffc
LcaZl <luca.zanolo@outlook.it>
2023-12-16 22:48:06 +0100

    Updates

--- DeliverooAgent/src/index.js
+++ DeliverooAgent/src/index.js
@@ -1,24 +1,43 @@
 import { Agent } from './agent/agent.js'
 import { configurations } from './config.js'
 
-const duration  = 100 // seconds
-const moveType = 'BFS' // BFS, PDDL, PDDL_BATCH
-// Number of option to calculate the path from the end of the previous, based on actual utility order.
-const lookAhead = 2 // For both PDDL and BFS
-const fastPick = false // Only for BFS, for now
-const changingRisk = 0.5 // Percentage penality in new option utility, if the actual is stopped for the new one.
-const adjustMovementCostWindow = 3000 // ms
-const multiagent = true
-const teamSize = 3
+// PARAMETERS
 
-const currentConfiguration = process.argv[2] - 1
+// Duration of the agent, if not Infinity the value will be used by the agent.
+const duration  = 100 // seconds
+
+// Two option: BFS or PDDL.
+const moveType = 'BFS'
+
+// Number of option to calculate the path from the end of the previous, based on actual utility order.
+const lookAhead = 2 // For both PDDL and BFS.
+
+const fastPick = false //  For both PDDL and BFS.
+
+// Percentage penality in new option utility, if the actual is stopped for the new one.
+const changingRisk = 0.5 
+
+// Time window to adjust the movemnt penality, used to calcul,ate the utility of each option.
+const adjustMovementCostWindow = 3000 //ms
+
+// Multiagent settings
+const multiagent = true // Enable the multiagent functionalities
+const teamSize = 3 // Size of the team
+
+// The agent configuration id must be specified with 'node index.js [ConfigurationIndex]'
+const currentConfigurationIndex = process.argv[2] - 1
+
+// The other configurations in config.js are the one used by other agents
+// Use them to get the team member names
 let teamNames = new Set()
 for (const [index, configuration] of configurations.entries()){
-    if (index !== currentConfiguration && teamNames.size < (teamSize - 1))
+    if (index !== currentConfigurationIndex && teamNames.size < (teamSize - 1))
         teamNames.add(configuration.username)
 }
-const {host, token, name} = configurations[currentConfiguration];
-console.log(teamNames)
+
+// Current agent configuration
+const {host, token, name} = configurations[currentConfigurationIndex];
+
 const agent = new Agent(
     host, 
     token, 
